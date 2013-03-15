84e8a2e3eaeeb435acae797267b431d82ec12bb3
Manfred Schwind <web@mani.de>
2013-03-15 12:17:26 +0100

    Update FTButton.m
    point is provided in local coordinates (self.bounds).
    Setting origin of self.frame to zero has the same effect in most cases, but it's not clean code.

--- FTButton.m
+++ FTButton.m
@@ -16,9 +16,7 @@
     if(UIEdgeInsetsEqualToEdgeInsets(self.hitTestEdgeInsets, UIEdgeInsetsZero) || !self.enabled || self.hidden) {
         return [super hitTest:point withEvent:event];
     }
-    // The point that is being tested is relative to self, so remove origin
-    CGRect relativeFrame = CGRectMake(0, 0, self.frame.size.width, self.frame.size.height);
-    CGRect hitFrame = UIEdgeInsetsInsetRect(relativeFrame, self.hitTestEdgeInsets);
+    CGRect hitFrame = UIEdgeInsetsInsetRect(self.bounds, self.hitTestEdgeInsets);
     if(CGRectContainsPoint(hitFrame, point)) {
         return self;
     }
